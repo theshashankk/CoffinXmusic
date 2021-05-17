34ea4daab403046eb9591c390dea4535f9ef3a4a
InukaAsith <inukaasith7@gmail.com>
2021-05-17 07:04:25 +0000

    Auto Fixes: Code formatted

--- CoffinXmusic/services/downloaders/youtube.py
+++ CoffinXmusic/services/downloaders/youtube.py
@@ -2,10 +2,9 @@
 
 from os import path
 
-from youtube_dl import YoutubeDL
-
 from DaisyXMusic.config import DURATION_LIMIT
 from DaisyXMusic.helpers.errors import DurationLimitError
+from youtube_dl import YoutubeDL
 
 ydl_opts = {
     "format": "bestaudio[ext=m4a]",
