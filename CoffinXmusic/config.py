fa6c0aaeb3319befd797614af0ac909d00dcdfcb
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-17 12:29:51 +0530

    Create config.py

---
+++ CoffinXmusic/config.py
@@ -0,0 +1,24 @@
+
+import os
+from os import getenv
+
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "@CoffinX_updates")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/dcfdf612e499eef0e0b1f.png")
+admins = {}
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+ARQ_API_KEY = getenv("ARQ_API_KEY", "VKSSQH-SRNJYZ-MSOJAI-QALESR-ARQ")
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
